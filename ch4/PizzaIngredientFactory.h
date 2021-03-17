380ea15aa981b6eebed0ce1a979089d0f8368254
unknown <yldbear77@gmail.com>
2021-03-17 16:29:12 +0900

    modified virtual function

--- ch4/PizzaIngredientFactory.h
+++ ch4/PizzaIngredientFactory.h
@@ -3,12 +3,14 @@
 
 #include "Ingredient.h"
 
+#include <vector>
+
 class PizzaIngredientFactory {
 public:
 	virtual Dough createDough() = 0;
 	virtual Sauce createSauce() = 0;
 	virtual Cheese createCheese() = 0;
-	//virtual Veggies[] createVeggies() = 0;
+	virtual std::vector<Veggies*> createVeggies() = 0;
 	virtual Pepperoni createPepperoni() = 0;
 	virtual Clams createClam() = 0;
 };
